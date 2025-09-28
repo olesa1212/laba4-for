39e13375f9cc10becc072773271390fb68cdb293
olesa1212 <o6635970@gmail.com>
2025-09-28 13:24:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите начальное значение");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите конечное число");
+int b= Convert.ToInt32(Console.ReadLine());
+int c = 0;
+for (int i = 0; i < b; i++)
+{
+    if (i % 12 == 0)
+    {
+        c++;
+    }
+}
+    Console.WriteLine(c);
